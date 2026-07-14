75f447e8d84eb2fd99c0b5655e159f519ae1fa8a
satwi-ratho21 <satwikaratho21@gmail.com>
2026-07-14 11:15:25 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
   return {
     server: {
       port: 3000,
-      host: '0.0.0.0',
+      host: "0.0.0.0",
 
       // Allow your Render domain
       allowedHosts: [
